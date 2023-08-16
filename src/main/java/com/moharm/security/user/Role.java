35d50444b90a20fbf4f0197e05d373caf275201b
048258 <mohcine.harmouch@intelcia.com>
2023-08-16 15:28:28 +0100

    define Roles

---
+++ src/main/java/com/moharm/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.moharm.security.user;
+
+public enum Role {
+
+  USER,ADMIN
+}
--- src/main/java/com/moharm/security/user/User.java
+++ src/main/java/com/moharm/security/user/User.java
@@ -1,13 +1,20 @@
 package com.moharm.security.user;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
 @Builder
@@ -15,7 +22,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "_user")
-public class User  {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue
@@ -25,5 +32,46 @@ public class User  {
   private String email;
   private String password;
 
+  //it well take the string value of the enum
+  @Enumerated(EnumType.STRING )
+  private Role role;
 
+
+  /**
+   * @return List of Roles
+   */
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
