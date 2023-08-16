6382db720a92646162e132fc7bca488de8cb77c6
048258 <mohcine.harmouch@intelcia.com>
2023-08-16 17:34:30 +0100

    add UserRepository

---
+++ src/main/java/com/moharm/security/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.moharm.security.repository;
+
+import com.moharm.security.user.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  Optional<User> findByEmail(String email);
+}
